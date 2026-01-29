cbceb91e5370226d3ccc8f884d7df914b28ed061
Matheus <matheus.g.silva@hotmail.com>
2026-01-29 14:41:26 -0300

    Update VaultMetadata.ts

--- packages/app/schemas/VaultMetadata.ts
+++ packages/app/schemas/VaultMetadata.ts
@@ -15,7 +15,7 @@ export const VaultMetadataSchema = z.object({
   isAutomated: z.boolean(),
   isHighlighted: z.boolean(),
   isPool: z.boolean(),
-  isStakingDisabled: z.boolean(),
+  shouldDisableStaking: z.boolean(),
   shouldUseV2APR: z.boolean(),
   migration: z.object({
     available: z.boolean(),
